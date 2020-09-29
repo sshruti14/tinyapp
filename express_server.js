c0445f0ec37aa14c4c0fa6825606f3230eaff1b4
Shruti Shetty <shetty_14august@yahoo.com>
2020-09-29 02:58:26 +0000

    Adding the rote to add new url and redirect to Myurls page

--- express_server.js
+++ express_server.js
@@ -9,6 +9,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//To generate a unique Id while adding an entry to DB
+function generateRandomString() {
+  let array = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let randString = "";
+
+  randString += array[Math.floor(Math.random() * array.length)];
+  array += "0123456789";
+
+  while (randString.length < 7) {
+    randString += array[Math.floor(Math.random() * array.length)];
+  }
+  return randString;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,22 +39,34 @@ app.get("/hello", (req, res) => {
 });
 
 //To display the list of urls
-app.get("/urls",(req,res) =>{
+app.get("/urls", (req, res) => {
   const templateVars = {
-    urls:urlDatabase 
-  }
-  res.render('urls_index',templateVars);
-})
+    urls: urlDatabase,
+  };
+  res.render("urls_index", templateVars);
+});
+
+//To render urls_new.ejs.ie to display the page to add a new url to tiny app
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 //To filter for each url
-app.get("/urls/:shortURL",(req,res) => {
+app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
-    shortURL:req.params.shortURL,
-    longURL : urlDatabase[req.params.shortURL]
-  }
-  res.render("urls_show",templateVars);
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
 
-})
+//To submit a form on creating a new URL
+app.post("/urls", (req, res) => {
+  let randString = generateRandomString();
+  urlDatabase[randString] =req.body.longURL
+  console.log(urlDatabase);
+  res.redirect('/urls'); 
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
